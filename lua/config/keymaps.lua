abd843a2f517e8f53173365104841b6f5838102f
Taejun Park <thecats1105@gmail.com>
2025-07-03 20:58:01 +0900

    refactor(keymaps): use `vim.diagnostic.jump()` instead of deprecated `vim.diagnostic.goto_next()`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -60,7 +60,7 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
-  vim.diagnostic.goto_next()
+  vim.diagnostic.jump({ count = 1, float = true })
 end, opts)
 
 keymap.set("n", "<leader>r", function()
